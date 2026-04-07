8df27d439fc369a0341ac63cd2a9bde67b6157de
Jacob Pichelmeyer <jacobpichelmeyer@gmail.com>
2026-04-07 01:24:49 -0400

    Update

--- courses/converter_lessons_html_to_json_2026_04_06_a.py
+++ courses/converter_lessons_html_to_json_2026_04_06_a.py
@@ -35,6 +35,7 @@ EXAMPLE: Condensed HTML
 import copy
 import os
 
+"""
 def read_file(path:str="lessons_pre_json.html"):
     with open(path, "r", encoding="utf-8") as file:
         lines = file.readlines()
@@ -46,4 +47,90 @@ for i in range(min(10, len(lines))):
     lc = copy.deepcopy(lines[i])
     lc = lc.strip()
     print(lc)
+"""
 
+class H2Hconverter():
+    
+    def __init__(self):
+        self.path = "lessons_pre_json.html"
+        pass
+    
+    def _clean_lines(self, lines:list=[], lines_max:int=-1):
+        lines_cleaned = []
+        counter = -1
+        
+        #lessons_all = []
+        #lessons_by_course = []
+        #lessons_by_unit = []
+        #lessons_by_beat = []
+        
+        
+        """
+        for i in range(len(lines)):
+            
+            counter += 1
+            if lines_max > -1:
+                if counter > lines_max:
+                    break
+            
+            line = lines[i].rstrip()
+            ws_l = len(line) - len(line.lstrip(' '))
+            line = line.lstrip()
+            #print(line)
+            
+            if '<div class="course-header">' in line:
+                print("Course found!")
+                
+            
+            elif '<div class="unit-header">' in line:
+                print("Unit found!")
+                
+            
+            elif '<div class="beat-header">' in line:
+                print("Beat found!")
+                
+                
+            
+        for course in lessons_all:
+            print("\n\n\n")
+            print("COURSE")
+            for unit in course:
+                print("\tUNIT")
+                for beat in unit:
+                    print("\t\tbeat")
+            
+            
+            
+            line_exposed = copy.deepcopy(lines[i])
+            print(1, line_exposed)
+            
+            line_exposed = line_exposed.rstrip()
+            print(2, line_exposed)
+            
+            whitespace_l = len(line_exposed) - len(line_exposed.lstrip(' '))
+            tabs_l = len(line_exposed) - len(line_exposed.lstrip("\t"))
+            print(3, line_exposed, " : whitespace_l : ", whitespace_l, " : tabs_l : ", tabs_l)
+            
+            #line_exposed = repr(line_exposed)
+            #print(4, line_exposed)
+            
+            print(5,"-----------------------------")
+        """
+            
+        return lines_cleaned
+    
+    def _read_file(self):
+        with open(self.path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+        return lines
+    
+    def convert(self):
+        lines = self._read_file()
+        self._clean_lines(lines=lines, lines_max=-1)
+    
+    
+if __name__ == "__main__":
+    print('__name__ == "__main__" syntax working')
+    converter = H2Hconverter()
+    converter.convert()
+    
